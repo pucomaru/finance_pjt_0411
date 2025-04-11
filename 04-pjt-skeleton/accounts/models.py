3a2c1e0aa3659b4f75e05c630bbfd785ce9c5536
EdwardLee <ghfy1245.dev@gmail.com>
2025-04-11 15:44:06 +0900

    작업 중간 저장: models.py 등

--- 04-pjt-skeleton/accounts/models.py
+++ 04-pjt-skeleton/accounts/models.py
@@ -1,15 +1,19 @@
-<<<<<<< HEAD
 import datetime
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
 
+# 사용자 모델 커스텀
 class User(AbstractUser):
     pass
 
+# 관심 종목 모델
 class StockInterest(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name = 'stock_interests')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='stock_interests'
+    )
     stock = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -20,8 +24,3 @@ class StockInterest(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.stock}"
-=======
-from django.db import models
-
-# Create your models here.
->>>>>>> dahye2-dev
